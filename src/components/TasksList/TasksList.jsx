83fe04e1dbb7c5efceaec82a897a4b3128b2313c
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-03 13:10:39 +0200

    fix(TasksList): fix task list not updating properly

--- src/components/TasksList/TasksList.jsx
+++ src/components/TasksList/TasksList.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import "./TasksList.css";
 
 export const TasksList = () => {
-	const tasks = ["Buy groceries", "Clean the house", "Finish homework", "Call the bank"];
+	const [tasks, setTasks] = useState(["Buy groceries", "Clean the house", "Finish homework", "Call the bank"]);
 
 	const renderTasks = (tasks) => {
-		tasks.forEach((task, index) => {
-			<li key={index}>{task}</li>;
+		return tasks.map((task, index) => {
+			return <li key={index}>{task}</li>;
 		});
+	};
+
+	const handleAddTasks = () => {
+		const newTask = addTask.trim();
+		if (newTask.length === 0) return;
+
+		setTasks((prevTasks) => [...prevTasks, newTask]);
+		setAddTask("");
+	};
+
+	const [addTask, setAddTask] = useState("");
+
+	const handleTask = (e) => {
+		setAddTask(e.target.value);
 	};
 
 	return (
 		<div className="container-tasks">
 			<ul className="tasks-list">{renderTasks(tasks)}</ul>
+			<label htmlFor="task-list">Add tasks:</label>
+			<input type="text" id="task-list" value={addTask} onChange={handleTask} />
+			<button onClick={handleAddTasks} type="submit">
+				Add
+			</button>
 		</div>
 	);
 };
