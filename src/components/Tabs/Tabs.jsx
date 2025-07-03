28a9013c6a911ea16d07e1f74ddc21e138f413dd
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-03 13:50:22 +0200

    refactor(Tabs): improve code Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -4,18 +4,18 @@ import "./Tabs.css";
 export const Tabs = () => {
 	const [activeTab, setActiveTab] = useState("home");
 
-	const handleActiveTab = (tab) => {
-		setActiveTab(tab);
-	};
+	// const handleActiveTab = (tab) => {
+	// 	setActiveTab(tab);
+	// };
 
 	return (
 		<div className="tabs">
 			{activeTab === "home" && <p>🏠</p>}
-			<button onClick={() => handleActiveTab("home")}>Home</button>
+			<button onClick={() => setActiveTab("home")}>Home</button>
 			{activeTab === "profile" && <p>👤</p>}
-			<button onClick={() => handleActiveTab("profile")}>Profile</button>
+			<button onClick={() => setActiveTab("profile")}>Profile</button>
 			{activeTab === "settings" && <p>⚙️</p>}
-			<button onClick={() => handleActiveTab("settings")}>Setting</button>
+			<button onClick={() => setActiveTab("settings")}>Setting</button>
 		</div>
 	);
 };
